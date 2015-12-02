e3f7b912387e54b2e3313f65963dd54b036147a3
Favenzio <fcalvo@fsu.edu>
2015-12-02 12:32:14 -0500

    Create summon-solr-search-results-page.tpl.php

---
+++ theme/summon-solr-search-results-page.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for search results page.
+ */
+?>
+
+<p>Your search for <strong><?php print $search_input; ?></strong>
+returned the following results:</p>
