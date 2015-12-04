7cc48ef39f4351ca52718cb44d32f6b1ede2ccb1
Favenzio Calvo <fcalvo@fsu.edu>
2015-12-04 00:05:50 +0000

    Created files for page results processor.c

--- js/summon_solr_search.js
+++ js/summon_solr_search.js
@@ -5,17 +5,16 @@
 
 /* Fixed Header */
 (function ($) {  
-  Drupal.behaviors.bento_box_search = {
+  Drupal.behaviors.summon_solr_search = {
     attach: function (context, settings) {            
      // All our js code here
-     $.each(Drupal.settings.bento_box_search, function(index, id_keywords) {
+     $.each(Drupal.settings.summon_solr_search, function(index, id_keywords) {
        var temp_url = id_keywords.split("|"); 
-       var bento_query_url = '/onesearch_query/' + temp_url[0] + '/' + temp_url[1];
+       var onesearch_query_url = '/onesearch_query/' + temp_url[0] + '/' + temp_url[1];
        $('#edit-onesearch-input').val(temp_url[1]);
        $.get(onesearch_query_url, function( results_html ) {
          $('#' + temp_url[0]).append(results_html);
          $('#ajax-loader-' + temp_url[0]).empty();
-         $('.onesearch-system-container').height(300).css({"overflow": "scroll"});
        });
     
      });
--- theme/summon-solr-search-results-page.tpl.php
+++ theme/summon-solr-search-results-page.tpl.php
@@ -7,3 +7,7 @@
 
 <p>Your search for <strong><?php print $search_input; ?></strong>
 returned the following results:</p>
+
+<div id="ajax-loader-1" class="ajax-loader">
+  <img src="<?php print '/' . drupal_get_path('module', 'summon_solr_search') . '/images/ajax-loader.gif' ?>">
+</div>
