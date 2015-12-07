41c48618c9839b28687a8814dc49b0e632192d87
Favenzio Calvo <fcalvo@fsu.edu>
2015-12-07 23:52:34 +0000

    Worked out workflow for processing queries.

--- theme/summon-solr-search-results-page.tpl.php
+++ theme/summon-solr-search-results-page.tpl.php
@@ -8,6 +8,8 @@
 <p>Your search for <strong><?php print $search_input; ?></strong>
 returned the following results:</p>
 
-<div id="ajax-loader-1" class="ajax-loader">
-  <img src="<?php print '/' . drupal_get_path('module', 'summon_solr_search') . '/images/ajax-loader.gif' ?>">
+<div id="1" class="search-container">
+  <div id="ajax-loader-1" class="ajax-loader">
+    <img src="<?php print '/' . drupal_get_path('module', 'summon_solr_search') . '/images/ajax-loader.gif' ?>">
+  </div>
 </div>
