af9bd7a1ddd7430fb98f9860efd30358c4b4ea00
Favenzio Calvo <fcalvo@fsu.edu>
2016-01-12 23:35:39 +0000

    Added tabbed searching for articles.

--- theme/summon-solr-search-results-page.tpl.php
+++ theme/summon-solr-search-results-page.tpl.php
@@ -8,8 +8,16 @@
 <p>Your search for <strong><?php print $search_input; ?></strong>
 returned the following results:</p>
 
-<div id="1" class="search-container">
+<div id="summon-search-menu">
+  <ul>
+    <li><a id="summon-search-all" href="#" class="ss-active">All</a></li>  
+    <li><a id="summon-search-articles" href="#">Articles</a></li>
+  </ul>
+</div>
+
+<div id="1" class="summon-search-container">
   <div id="ajax-loader-1" class="ajax-loader">
-    <img src="<?php print '/' . drupal_get_path('module', 'summon_solr_search') . '/images/ajax-loader.gif' ?>">
+    <p><img src="<?php print '/' . drupal_get_path('module', 'summon_solr_search') 
+      . '/images/ajax-loader.gif' ?>"></p>
   </div>
 </div>
