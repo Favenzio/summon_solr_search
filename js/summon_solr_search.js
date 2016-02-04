ae5602ce54b3c58f8a6563c8c09e27a71aae7d00
Bryan Brown <bryjbrown@gmail.com>
2016-02-04 15:43:48 -0500

    Fixed javascript loading image to appear on all tabs.

--- js/summon_solr_search.js
+++ js/summon_solr_search.js
@@ -18,66 +18,78 @@
         //all search handler
         $('#summon-search-all').click(function() {
           $('#1').empty();
+          $('#1').append('<div id="load-wait"><img src="/sites/all/modules/summon_solr_search/images/ajax-loader.gif"></div>');
           $("#summon-search-menu>ul>li>a.ss-active").removeClass("ss-active");
           $("#summon-search-all").addClass("ss-active");
           var article_query_url = '/onesearch_query/1/' + temp_url[1];
           $.get(article_query_url, function( results_html ) {
             $('#' + temp_url[0]).append(results_html);
+            $('#load-wait').empty();
           });
         });
         
         //article search handler
         $('#summon-search-articles').click(function() {
           $('#1').empty();
+          $('#1').append('<div id="load-wait"><img src="/sites/all/modules/summon_solr_search/images/ajax-loader.gif"></div>');
           $("#summon-search-menu>ul>li>a.ss-active").removeClass("ss-active");
           $("#summon-search-articles").addClass("ss-active");
           var article_query_url = '/onesearch_query/2/' + temp_url[1];
           $.get(article_query_url, function( results_html ) {
             $('#' + temp_url[0]).append(results_html);
+            $('#load-wait').empty();
           });
         });      
 
         //book search handler
         $('#summon-search-books').click(function() {
           $('#1').empty();
+          $('#1').append('<div id="load-wait"><img src="/sites/all/modules/summon_solr_search/images/ajax-loader.gif"></div>');
           $("#summon-search-menu>ul>li>a.ss-active").removeClass("ss-active");
           $("#summon-search-books").addClass("ss-active");
           var article_query_url = '/onesearch_query/3/' + temp_url[1];
           $.get(article_query_url, function( results_html ) {
             $('#' + temp_url[0]).append(results_html);
+            $('#load-wait').empty();
           });
         });
         
         //journal search handler
         $('#summon-search-journals').click(function() {
           $('#1').empty();
+          $('#1').append('<div id="load-wait"><img src="/sites/all/modules/summon_solr_search/images/ajax-loader.gif"></div>');
           $("#summon-search-menu>ul>li>a.ss-active").removeClass("ss-active");
           $("#summon-search-journals").addClass("ss-active");
           var article_query_url = '/onesearch_query/4/' + temp_url[1];
           $.get(article_query_url, function( results_html ) {
             $('#' + temp_url[0]).append(results_html);
+            $('#load-wait').empty();
           });
         });
         
         //database search handler
         $('#summon-search-databases').click(function() {
           $('#1').empty();
+          $('#1').append('<div id="load-wait"><img src="/sites/all/modules/summon_solr_search/images/ajax-loader.gif"></div>');
           $("#summon-search-menu>ul>li>a.ss-active").removeClass("ss-active");
           $("#summon-search-databases").addClass("ss-active");
           var article_query_url = '/onesearch_query/5/' + temp_url[1];
           $.get(article_query_url, function( results_html ) {
             $('#' + temp_url[0]).append(results_html);
+            $('#load-wait').empty();
           });
         });
         
         //website search handler
         $('#summon-search-website').click(function() {
           $('#1').empty();
+          $('#1').append('<div id="load-wait"><img src="/sites/all/modules/summon_solr_search/images/ajax-loader.gif"></div>');
           $("#summon-search-menu>ul>li>a.ss-active").removeClass("ss-active");
           $("#summon-search-website").addClass("ss-active");
           var article_query_url = '/onesearch_query/6/' + temp_url[1];
           $.get(article_query_url, function( results_html ) {
             $('#' + temp_url[0]).append(results_html);
+            $('#load-wait').empty();
           });
         }); 
       });
